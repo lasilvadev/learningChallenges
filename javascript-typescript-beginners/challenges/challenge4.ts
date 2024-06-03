cc5032623aa090ab6c93e84a01dd2f338614eec0
LaSilvaDev <mslarissasilva@gmail.com>
2024-06-03 12:22:48 -0400

    Update challenge4.ts

--- javascript-typescript-beginners/challenges/challenge4.ts
+++ javascript-typescript-beginners/challenges/challenge4.ts
@@ -25,5 +25,26 @@ const calculate = (num1: number, num2: number, operator: Operator): number | Cal
     return "Invalid operator";
 }
 
+const result = calculate(4, 5, "add"); 
+console.log(result);
+
 
 // Challenge 2: Write a function that takes two numbers as inputs and returns a string indicating their relationship. The possible relationships are equal, not equal, greater than, less than, or invalid. If either of the inputs is not a number, the function should return "invalid".
+
+type Relationship = "equal" | "not-equal" | "greater-than" | "less-than" | "invalid";
+
+const compareNumbers = (n1: number, n2: number): Relationship => {
+    if (typeof n1 !== "number" || typeof n2 !== "number") {
+        return "invalid";
+    }
+    if (n1 === n2) {
+        return "equal";
+    }
+    if (n1 > n2) {
+        return "greater-than"
+    }
+    return "less-than";
+}
+
+const relation = compareNumbers(1, 2);
+console.log(relation);
